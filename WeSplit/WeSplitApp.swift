e3d4d4562ff6b55ae2a85519d30e04075b1dd83c
walker-sean <walker.se@northeastern.edu>
2023-07-09 16:17:43 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sean Walker on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
