ba86785db61e1cb53754bd2da79722608cbbe599
walker-sean <walker.se@northeastern.edu>
2023-07-10 19:04:57 -0400

    Bill amount text field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: Double = 0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [0, 10, 15, 18, 20, 25]
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                }
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
         }
     }
 }
